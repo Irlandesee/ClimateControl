854cfa4b21385692d9d71f265dd25c91132e613e
Samaki01 <andrea.quaglia01@gmail.com>
2023-03-30 16:27:16 +0200

    Update climateMonitoringApp.java

--- climatemonitoring/src/main/java/com/climatemonitoring/climatemonitoring/climateMonitoringApp.java
+++ climatemonitoring/src/main/java/com/climatemonitoring/climatemonitoring/climateMonitoringApp.java
@@ -12,7 +12,7 @@ public class climateMonitoringApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(climateMonitoringApp.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hello World!");
         stage.setScene(scene);
         stage.show();
     }
