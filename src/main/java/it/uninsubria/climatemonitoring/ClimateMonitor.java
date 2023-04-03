2eb0bbd4def6d7e4d6af9308b0b091f2c580b52c
Samaki01 <andrea.quaglia01@gmail.com>
2023-04-03 21:24:24 +0200

    Aggiunge interfaccia da linea di comando.

--- src/main/java/it/uninsubria/climatemonitoring/ClimateMonitor.java
+++ src/main/java/it/uninsubria/climatemonitoring/ClimateMonitor.java
@@ -8,7 +8,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 /**
@@ -27,29 +29,108 @@ public class ClimateMonitor extends Application {
         HashMap<String, Operatore> operatoriAutorizzatiCache = dbRef.readOperatoriAutorizzatiFile();
         HashMap<String, Operatore> operatoriRegistratiCache = dbRef.readOperatoriRegistratiFile();
 
-        String email00 = "example00@climateMonitor.it";
-        String email01 = "example01@climateMonitor.it";
-        String email02 = "example10@climateMonitor.it";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean loggedIn = false;
 
-        debug("Registrazione in corso...");
-        signUp(dbRef, operatoriAutorizzatiCache, operatoriRegistratiCache, email00);
-        signUp(dbRef, operatoriAutorizzatiCache, operatoriRegistratiCache, email01);
-        signUp(dbRef, operatoriAutorizzatiCache, operatoriRegistratiCache, email02);
+        while (!loggedIn) {
+            System.out.println("""
+                    Benvenuto, digitare 'cerca' per visualizzare le aree di interesse disponibili.
+                    Digitare 'login' per effettuare il login (solo operatori registrati).
+                    Digitare 'registrazione' per effettuare la registrazione all'applicazione\s
+                    (solo operatori autorizzati).
+                    Digitare 'uscita' per terminare il programma.""");
+            switch (reader.readLine()) {
+                case "cerca" -> {
+                    printCache(geonamesCache);
+                    System.out.println("\nE' possibile visualizzare i dati climatici di un area di interesse digitando" +
+                            " il suo geonameID");
+                    String geonameID = reader.readLine();
+                    City areaGeografica;
+                    while ((areaGeografica = geonamesCache.get(geonameID)) == null) {
+                        System.out.println("GeonameID inserito non valido. Riprovare.");
+                        geonameID = reader.readLine();
+                    }
+                    System.out.println("Dati climatici di: " + areaGeografica);
+                }
+                case "login" -> {
+                    while (!loggedIn) {
+                        System.out.println("Digitare l'userID:");
+                        String userID = reader.readLine();
+                        if (userID.equals(""))
+                            break;
+                        String password;
+                        String enteredPassword;
+                        for (String key : operatoriRegistratiCache.keySet()) {
+                            Operatore operatore = operatoriRegistratiCache.get(key);
+                            if (userID.equals(operatore.getUserID())) {
+                                password = operatore.getPassword();
+                                System.out.println("Digitare la password:");
+                                while (!loggedIn) {
+                                    enteredPassword = reader.readLine();
+                                    if (enteredPassword.equals(""))
+                                        break;
+                                    if (password.equals(enteredPassword)) {
+                                        System.out.println("Accesso effettuato!\nBenvenuto " + operatore.getCognome()
+                                                + " " + operatore.getNome());
+                                        loggedIn = true;
+                                    } else {
+                                        System.out.println("Password errata!");
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                        if(!loggedIn)
+                            System.out.println("userID errato!");
+                    }
+                }
+                case "registrazione" -> {
+                    String cognome, nome, codiceFiscale, email, userID, password, centroAfferenza;
+                    System.out.println("Digitare il cognome:");
+                    cognome = reader.readLine();
+                    System.out.println("Digitare il nome:");
+                    nome = reader.readLine();
+                    System.out.println("Digitare il codice fiscale:");
+                    codiceFiscale = reader.readLine();
+                    System.out.println("Digitare l'email aziendale:");
+                    email = reader.readLine();
+                    if(operatoriRegistratiCache.containsKey(email)) {
+                        System.out.println("Utente gia' registrato!");
+                        break;
+                    }
+                    if(!operatoriAutorizzatiCache.containsKey(email)) {
+                        System.out.println("Email non valida!");
+                        break;
+                    }
+                    System.out.println("Digitare l'userID:");
+                    userID = reader.readLine();
+                    System.out.println("Digitare la password:");
+                    password = reader.readLine();
+                    System.out.println("Digitare il geonameID del centro di Afferenza:");
+                    centroAfferenza = reader.readLine();
+                    Operatore operatore = new Operatore(cognome, nome, codiceFiscale, email, userID,
+                            password, centroAfferenza);
+                    operatoriRegistratiCache.put(email, operatore);
+                    loggedIn = true;
+                    dbRef.registraOperatore(operatore);
+                    System.out.println("Accesso effettuato!\nBenvenuto " + cognome + " " + nome + "\n");
+                }
+                case "uscita" -> {
+                    System.exit(0);
+                }
+            }
+        }
 
-        debug("Stampa cache coordinate monitoraggio...");
-        printCache(geonamesCache);
-        debug("Stampa cache coordinate monitoraggio completata.\n");
-
-        debug("Stampa cache operatori autorizzati...");
-        printCache(operatoriAutorizzatiCache);
-        debug("Stampa cache operatori autorizzati completata.\n");
-
-        debug("Stampa cache operatori registrati...");
-        printCache(operatoriRegistratiCache);
-        debug("Stampa cache operatori registrati completata.\n");
-
-        dbRef.writeCoordinateMonitoraggioFile(geonamesCache);
-        debug("Copia sul file CoordinateMonitoraggio.dati completata.\n");
+//        debug("Stampa cache operatori autorizzati...");
+//        printCache(operatoriAutorizzatiCache);
+//        debug("Stampa cache operatori autorizzati completata.\n");
+//
+//        debug("Stampa cache operatori registrati...");
+//        printCache(operatoriRegistratiCache);
+//        debug("Stampa cache operatori registrati completata.\n");
+//
+//        dbRef.writeCoordinateMonitoraggioFile(geonamesCache);
+//        debug("Copia sul file CoordinateMonitoraggio.dati completata.\n");
     }
 
     /**
