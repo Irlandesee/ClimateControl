5d7546fdab64e4a206aff8f2bcc1b9306d009445
Mattia Lunardi <41571493+Irlandesee@users.noreply.github.com>
2023-04-04 10:51:40 +0200

    Added fields and getters to AreaInteresse

--- climatemonitoring/src/main/java/it/uninsubria/climatemonitoring/areaInteresse/AreaInteresse.java
+++ climatemonitoring/src/main/java/it/uninsubria/climatemonitoring/areaInteresse/AreaInteresse.java
@@ -3,7 +3,26 @@ package it.uninsubria.climatemonitoring.areaInteresse;
 public class AreaInteresse {
 
     private String areaID;
+    private String denominazione;
+    private String stato;
+    private float latitude;
+    private float longitude;
+
     public AreaInteresse(String areaID){
         this.areaID = areaID;
+    }
+    public String getAreaID(){return this.areaID;}
+    public String getDenominazione(){return this.denominazione;}
+    public String getStato(){return this.stato;}
+    public float getLatitude(){return this.latitude;}
+    public float getLongitude(){return this.longitude;}
+
+    public boolean equals(Object o){
+        //TODO:
+        return false;
+    }
+    public String toString(){
+        //TODO: return stringa rappresentante l'area di interesse
+        return null;
     }
 }
