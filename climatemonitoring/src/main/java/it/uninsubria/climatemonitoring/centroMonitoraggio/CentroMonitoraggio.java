b006802aff0cff48144a71fdc206652f9aaf8b92
Mattia Lunardi <41571493+Irlandesee@users.noreply.github.com>
2023-04-04 10:54:06 +0200

    Added getters and setters to CentroMonitoraggio

--- climatemonitoring/src/main/java/it/uninsubria/climatemonitoring/centroMonitoraggio/CentroMonitoraggio.java
+++ climatemonitoring/src/main/java/it/uninsubria/climatemonitoring/centroMonitoraggio/CentroMonitoraggio.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 
 public class CentroMonitoraggio {
 
-
     private String centroID;
     private String nomeCentro;
     private String via; //via-piazza
@@ -15,12 +14,72 @@ public class CentroMonitoraggio {
     private String comune;
     private String provincia;
 
+    //key: String => areaID
     private HashMap<String, AreaInteresse> areeInteresse;
 
     public CentroMonitoraggio(String centroID){
         this.centroID = centroID;
     }
 
+    public String getCentroID() {
+        return centroID;
+    }
+
+    public void setCentroID(String centroID) {
+        this.centroID = centroID;
+    }
+
+    public String getNomeCentro() {
+        return nomeCentro;
+    }
+
+    public void setNomeCentro(String nomeCentro) {
+        this.nomeCentro = nomeCentro;
+    }
+
+    public String getVia() {
+        return via;
+    }
+
+    public void setVia(String via) {
+        this.via = via;
+    }
+
+    public short getNumCivico() {
+        return numCivico;
+    }
+
+    public void setNumCivico(short numCivico) {
+        this.numCivico = numCivico;
+    }
+
+    public int getCap() {
+        return cap;
+    }
+
+    public void setCap(int cap) {
+        this.cap = cap;
+    }
+
+    public String getComune() {
+        return comune;
+    }
+
+    public void setComune(String comune) {
+        this.comune = comune;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public HashMap<String, AreaInteresse> getAreeInteresse() {
+        return areeInteresse;
+    }
 
     public String toString(){
         //TODO: return new string representing this instance
