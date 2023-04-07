e012fc6e5ea874c3cf1def4491867a22f353040f
Samaki01 <andrea.quaglia01@gmail.com>
2023-04-07 20:47:08 +0200

    Aggiunge check sull'input.

--- src/main/java/it/uninsubria/climatemonitoring/ClimateMonitor.java
+++ src/main/java/it/uninsubria/climatemonitoring/ClimateMonitor.java
@@ -6,12 +6,14 @@ import it.uninsubria.climatemonitoring.parametriClimatici.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Locale;
 
 /**
  * @author : Mattia Mauro Lunardi, 736898, mmlunardi@studenti.uninsubria.it, VA
@@ -42,7 +44,9 @@ public class ClimateMonitor {
         while (true) {
             while (loggedOperator == null) {
                 System.out.println("""
+                        
                         Benvenuto!
+                        
                         Digitare 'cerca' per visualizzare le aree di interesse disponibili.
                         Digitare 'login' per effettuare il login (solo operatori registrati).
                         Digitare 'registrazione' per effettuare la registrazione all'applicazione\s
@@ -67,12 +71,14 @@ public class ClimateMonitor {
 
             while (loggedOperator != null) {
                 CentroMonitoraggio centroAfferenza = loggedOperator.getCentroAfferenza();
-                System.out.println("Area riservata - Centro di monitoraggio " + centroAfferenza.getNomeCentro() + "\n");
+                System.out.println("\nArea riservata - Centro di monitoraggio " + centroAfferenza.getNomeCentro() + "\n");
                 System.out.println("""
                         Digitare 'aggiungi' per aggiungere un aree di interesse al centro di monitoraggio.
+                        Digitare 'cerca' per visualizzare le aree di interesse disponibili.
                         Digitare 'logout' per effettuare il logout e tornare al menu' principale.
                         Digitare 'uscita' per terminare il programma.
                         Digitare 'inserisci' per inserire i dati relativi ad una delle aree di interesse.
+                        
                         Aree di interesse registrate:""");
                 printCache(loggedOperator.getCentroAfferenza().getAreeInteresse());
 
@@ -109,56 +115,82 @@ public class ClimateMonitor {
             System.out.println("Area di interesse non trovata!");
             return;
         }
-        System.out.println("Area di interesse scelta:\n" + areaInteresse);
+        System.out.println("Area di interesse scelta:\n" + areaInteresse + "\n");
         LinkedList<ParametroClimatico> parametriClimatici = new LinkedList<>();
         LinkedList<Integer> punteggi = new LinkedList<>();
         LinkedList<String> note = new LinkedList<>();
-        ArrayList<String> nomiParametri = new ArrayList<>(Arrays.asList("al Vento", "all'Umidità", "alla Pressione",
+        ArrayList<String> nomiParametri = new ArrayList<>(Arrays.asList("al Vento", "all'Umidita", "alla Pressione",
                 "alla Temperatura", "alle Precipitazioni", "all'Altitudine dei ghiacciai", "alla Massa dei ghiacciai"));
 
-        //TODO verifica interi in lettura e max caratteri.
         for(int i = 0; i < 7; i++) {
             System.out.println("Digitare il punteggio relativo " +
                     nomiParametri.get(i) + "(da 1 critico a 5 ottimale, 0 per nessun inserimento):");
             String punteggio = reader.readLine();
-            int punteggioNumerico = 0;
-            if (isNumeric(punteggio)) {
-                punteggioNumerico = Integer.parseInt(punteggio);
-            } else {
+            while (isNotNumeric(punteggio) || Integer.parseInt(punteggio) < 0 || Integer.parseInt(punteggio) > 5) {
                 System.err.println("Il punteggio deve essere un numero compreso tra 1-critico e 5-ottimale!");
-                return;
-            }
-            if(punteggioNumerico < 0 || punteggioNumerico > 5) {
-                System.err.println("Il punteggio deve essere un numero compreso tra 1-critico e 5-ottimale!");
-                return;
+                System.out.println("Digitare il punteggio relativo " +
+                        nomiParametri.get(i) + "(da 1 critico a 5 ottimale, 0 per nessun inserimento):");
+                punteggio = reader.readLine();
             }
             punteggi.add(i, Integer.parseInt(punteggio));
+
+            if (Integer.parseInt(punteggio) == 0) {
+                note.add(i, "");
+                continue;
+            }
             System.out.println("Digitare le note relative " + nomiParametri.get(i) + " (massimo 256 caratteri):");
             String nota = reader.readLine();
-            if (nota.length() > 256) {
+            while (nota.length() > 256) {
                 System.err.println("Nota troppo lunga!\nLa nota deve essere al massimo di 256 caratteri!");
-                return;
+                System.out.println("Digitare le note relative " + nomiParametri.get(i) + " (massimo 256 caratteri):");
+                nota = reader.readLine();
             }
             note.add(i, nota);
         }
 
-        //TODO verify date input
         String annoRilevazione;
         String meseRilevazione;
         String giornoRilevazione;
-        System.out.println("Digitare la l'anno di rilevazione:");
-        annoRilevazione = reader.readLine();
-        System.out.println("Digitare il mese di rilevazione:");
-        meseRilevazione = reader.readLine();
-        if(meseRilevazione.length() == 1)
-            meseRilevazione = "0" + meseRilevazione;
-        System.out.println("Digitare il giorno di rilevazione:");
-        giornoRilevazione = reader.readLine();
-        if(giornoRilevazione.length() == 1)
-            giornoRilevazione = "0" + giornoRilevazione;
-        String string = giornoRilevazione + " " + meseRilevazione + " " + annoRilevazione;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy", Locale.ITALIAN);
-        LocalDate date = LocalDate.parse(string, formatter);
+        String data;
+        final String DATE_FORMAT = "dd-MM-yyyy";
+
+        do {
+            System.out.println("Digitare la l'anno di rilevazione:");
+            annoRilevazione = reader.readLine();
+            while (isNotNumeric(annoRilevazione)) {
+                System.out.println("Anno inserito non valido! L'anno deve essere un numero intero.");
+                System.out.println("Digitare la l'anno di rilevazione:");
+                annoRilevazione = reader.readLine();
+            }
+
+            System.out.println("Digitare il mese di rilevazione:");
+            meseRilevazione = reader.readLine();
+            while (isNotNumeric(meseRilevazione) || Integer.parseInt(meseRilevazione) < 1 ||
+                    Integer.parseInt(meseRilevazione) > 12) {
+                System.out.println("Mese inserito non valido! Il mese deve essere un numero intero compreso tra 1 e 12.");
+                System.out.println("Digitare la il mese di rilevazione:");
+                meseRilevazione = reader.readLine();
+            }
+            if (meseRilevazione.length() == 1)
+                meseRilevazione = "0" + meseRilevazione;
+
+            System.out.println("Digitare il giorno di rilevazione:");
+            giornoRilevazione = reader.readLine();
+            while (isNotNumeric(giornoRilevazione) || Integer.parseInt(giornoRilevazione) < 1 ||
+                    Integer.parseInt(giornoRilevazione) > 31) {
+                System.out.println("Giorno inserito non valido! Il giorno deve essere un " +
+                        "numero intero compreso tra 1 e 31");
+                System.out.println("Digitare il giorno di rilevazione:");
+                giornoRilevazione = reader.readLine();
+            }
+            if (giornoRilevazione.length() == 1)
+                giornoRilevazione = "0" + giornoRilevazione;
+
+            data = giornoRilevazione + "-" + meseRilevazione + "-" + annoRilevazione;
+            if(isDateInvalid(data))
+                System.out.println("Data non valida! Riprova.");
+        } while (isDateInvalid(data));
+        LocalDate date = LocalDate.parse(data, DateTimeFormatter.ofPattern(DATE_FORMAT));
 
         parametriClimatici.add(0, new Vento(punteggi.get(0), note.get(0), date));
         parametriClimatici.add(1, new Umidita(punteggi.get(1), note.get(1), date));
@@ -168,18 +200,32 @@ public class ClimateMonitor {
         parametriClimatici.add(5, new AltitudineGhiacciai(punteggi.get(5), note.get(5), date));
         parametriClimatici.add(6, new MassaGhiacciai(punteggi.get(6), note.get(6), date));
 
+        //TODO bug rimuove l'area di interesse registrata dopo che si inseriscono i dati
+        // non sono riuscito a replicare il bug
         areaInteresse.addParametriClimatici(parametriClimatici);
         dbRef.writeCentriMonitoraggioFile(centriMonitoraggioCache);
         dbRef.writeAreeInteresseFile(areeInteresseAssociateCache);
         System.out.println("Parametri climatici aggiunti con successo!");
     }
 
-    public static boolean isNumeric(String str) {
+    public static boolean isDateInvalid(String date) {
+        final String DATE_FORMAT = "dd-MM-yyyy";
+        try {
+            DateFormat df = new SimpleDateFormat(DATE_FORMAT);
+            df.setLenient(false);
+            df.parse(date);
+            return false;
+        } catch (ParseException e) {
+            return true;
+        }
+    }
+
+    public static boolean isNotNumeric(String str) {
         try {
             Double.parseDouble(str);
-            return true;
-        } catch(NumberFormatException e){
             return false;
+        } catch(NumberFormatException e){
+            return true;
         }
     }
 
@@ -278,7 +324,7 @@ public class ClimateMonitor {
 
         System.out.println("Digitare il numero civico del centro di afferenza:");
         numeroCivico = reader.readLine();
-        while (!isNumeric(numeroCivico) || Integer.parseInt(numeroCivico) < 1) {
+        while (isNotNumeric(numeroCivico) || Integer.parseInt(numeroCivico) < 1) {
             System.out.println("Il numero civico deve essere un numero maggiore di 0!");
             System.out.println("Digitare il numero civico del centro di afferenza:");
             numeroCivico = reader.readLine();
@@ -287,7 +333,7 @@ public class ClimateMonitor {
 
         System.out.println("Digitare il CAP del centro di afferenza:");
         cap = reader.readLine();
-        while (!isNumeric(cap) || Integer.parseInt(cap) < 1) {
+        while (isNotNumeric(cap) || Integer.parseInt(cap) < 1) {
             System.out.println("Il CAP deve essere un numero maggiore di 0!");
             System.out.println("Digitare il CAP del centro di afferenza:");
             cap = reader.readLine();
