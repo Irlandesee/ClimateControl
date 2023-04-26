c2ef7db857d6c6303ca1130a2b622bbe6bf0a018
Mattia Lunardi <41571493+Irlandesee@users.noreply.github.com>
2023-04-26 16:40:00 +0200

    Added parameter generation

--- climatemonitoring/src/main/java/it/uninsubria/climatemonitoring/climateParameters/ClimateParameter.java
+++ climatemonitoring/src/main/java/it/uninsubria/climatemonitoring/climateParameters/ClimateParameter.java
@@ -1,5 +1,8 @@
 package it.uninsubria.climatemonitoring.climateParameters;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 import java.time.LocalDate;
 
@@ -59,6 +62,35 @@ public class ClimateParameter {
     private String massaGhiacciaiNotes;
 
     private HashMap<String, Short> paramValues;
+
+    public static String generateParameterID(){
+        //generates random alphanumeric string
+        int leftLimit = 48; //0
+        int rightLimit = 122; //z
+        int targetStringLength = 10;
+        Random ran = new Random();
+        String input = ran.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i>= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        //generates md5Hash
+        String hashText = null;
+        try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            hashText = convertToHex(messageDigest);
+        }catch(NoSuchAlgorithmException nsae){nsae.printStackTrace();}
+        return hashText;
+    }
+
+    private static String convertToHex(final byte[] messageDigest){
+        BigInteger bigint = new BigInteger(1, messageDigest);
+        String hexText = bigint.toString(16);
+        while(hexText.length() < 32) hexText = "0".concat(hexText);
+        return hexText;
+    }
 
     public ClimateParameter(String parameterID){
         this.parameterID = parameterID;
