6dc240ba8f542a11a4ac162cbd1b1309a1e8d356
Mattia Lunardi <41571493+Irlandesee@users.noreply.github.com>
2023-04-27 14:47:10 +0200

    Created IDGenerator
    Class used for generating random ids

---
+++ climatemonitoring/src/main/java/it/uninsubria/climatemonitoring/util/IDGenerator.java
@@ -0,0 +1,39 @@
+package it.uninsubria.climatemonitoring.util;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+public class IDGenerator {
+
+    public static String generateParameterID(){
+        //generates random alphanumeric string
+        int leftLimit = 48; //0
+        int rightLimit = 122; //z
+        int targetStringLength = 10;
+        Random ran = new Random();
+        String input = ran.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i>= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        //generates md5Hash
+        String hashText = null;
+        try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            hashText = convertToHex(messageDigest);
+        }catch(NoSuchAlgorithmException nsae){nsae.printStackTrace();}
+        return hashText;
+    }
+
+    private static String convertToHex(final byte[] messageDigest){
+        BigInteger bigint = new BigInteger(1, messageDigest);
+        String hexText = bigint.toString(16);
+        while(hexText.length() < 32) hexText = "0".concat(hexText);
+        return hexText;
+    }
+
+}
